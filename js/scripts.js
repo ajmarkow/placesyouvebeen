b54254b59881ea12258c12cb7837f882940513ce
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-24 09:20:09 -0700

    Create Destination Object.
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function destination(locationName,landMarks,dateVisited,rating,notes {
+  this.locationName = locationName
+  this.landMarks = landMarks
+  this.dateVisited = dateVisited
+  this.rating = rating
+  this.notes = notes
+}
